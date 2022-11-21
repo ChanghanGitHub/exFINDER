1993268f8e631f8956d189c74c9ee43e443c0e15
ChanghanGitHub <changhanhe@gmail.com>
2022-11-21 07:14:42 -0800

    Update: vignette1, .rds files

---
+++ R/data.R
@@ -0,0 +1,71 @@
+#' Ligand-receptor interactions in exFINDER database for zebrafish
+#'
+#' @format A dataframe includes the ligand-receptor interactions
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"LR_layer1_zebrafish"
+
+#' Receptor-TF signaling in exFINDER database for zebrafish
+#'
+#' @format A dataframe includes the Receptor-TF signaling
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"RTF_layer2_zebrafish"
+
+#' TF-Target regulation in exFINDER database for zebrafish
+#'
+#' @format A dataframe includes the TF-Target regulations
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"TFT_layer3_zebrafish"
+
+#' Ligand-receptor interactions in exFINDER database for human
+#'
+#' @format A dataframe includes the ligand-receptor interactions
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"LR_layer1_human"
+
+#' Receptor-TF signaling in exFINDER database for human
+#'
+#' @format A dataframe includes the Receptor-TF signaling
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"RTF_layer2_human"
+
+#' TF-Target regulation in exFINDER database for human
+#'
+#' @format A dataframe includes the TF-Target regulations
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"TFT_layer3_human"
+
+#' Ligand-receptor interactions in exFINDER database for mouse
+#'
+#' @format A dataframe includes the ligand-receptor interactions
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"LR_layer1_mouse"
+
+#' Receptor-TF signaling in exFINDER database for mouse
+#'
+#' @format A dataframe includes the Receptor-TF signaling
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"RTF_layer2_mouse"
+
+#' TF-Target regulation in exFINDER database for mouse
+#'
+#' @format A dataframe includes the TF-Target regulations
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"TFT_layer3_mouse"
+
+#' Ligand-receptor interactions in CellChat database for human
+#'
+#' @format A dataframe includes the ligand-receptor interactions
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"CellChatLR.human"
+
+#' Ligand-receptor interactions in CellChat database for mouse
+#'
+#' @format A dataframe includes the ligand-receptor interactions
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"CellChatLR.mouse"
+
+#' Ligand-receptor interactions in CellChat database for zebrafish
+#'
+#' @format A dataframe includes the ligand-receptor interactions
+#' @source \url{https://github.com/ChanghanGitHub/exFINDER/}
+"CellChatLR.zebrafish"
